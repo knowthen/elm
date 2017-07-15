12fe51d012463003955bfec4bd13022781cd9755
Chase Pursley <chasepursley@gmail.com>
2017-07-15 13:35:34 +0200

    [EXERCISES] 02: Complete 1, 2 & 3.

---
+++ 02 more on functions/Exercise2.elm
@@ -0,0 +1,25 @@
+module Main exposing (..)
+
+import Html
+import String
+
+
+(~=) str1 str2 =
+    let
+        stringOne =
+            str1 |> String.slice 0 1
+
+        stringTwo =
+            str2 |> String.slice 0 1
+    in
+    stringOne |> String.startsWith stringTwo
+
+
+stringStartsWith =
+    (~=) "chase" "xchase"
+
+
+main =
+    stringStartsWith
+        |> toString
+        |> Html.text
